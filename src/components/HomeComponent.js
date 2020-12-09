c45aabee2ce3d74bea680a0a5bcf08866e347312
mazibinfo <mazibulyah@gmail.com>
2020-12-09 14:45:26 +0600

    Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle, CardSubtitle } from 'reactstrap'
+
+function RenderCard({item}) {
+    return(
+        <Card>
+            <CardImg src={item.image} alt={item.name}></CardImg>
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                <CardSubtitle>{item.designation ? item.designation: null}</CardSubtitle>
+                <CardText>{item.description}</CardText>                
+            </CardBody>
+        </Card>
+    );
+}
 
 function Home(props) {
     return (
         <div className='container'>
-            <h4>Home</h4>
+            <div className='row align-items-start'>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.dish} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.leader} />
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,13 @@
-import Menu from './MenuComponent';
-import DishDetails from './DishDetailsComponent';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes';
 import { Component } from 'react';
 import Home from './HomeComponent';
+import Menu from './MenuComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Contact from './ContactComponent';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { COMMENTS } from '../shared/commnets';
 import { Switch, Redirect, Route } from 'react-router-dom';
 
 
@@ -13,19 +16,21 @@ class Main extends Component {
             super(props);
             this.state = {
             dishes: DISHES,
-            selectedDish: null
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
+            commnets: COMMENTS
         };
     }
-
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId})
-    }
-
 
     render() {        
         const HomePage = () => {
             return(
-                <Home />
+                <Home 
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                    commnet={this.state.commnets.filter((commnet) =>commnet.featured)[0]}
+                    promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+                />
             );
         }
         return (
@@ -34,6 +39,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
